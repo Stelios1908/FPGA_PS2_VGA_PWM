a7ef53ae3f9b9ccecaf1c8aba25a7e577cef03c8
Stylianos Sideris <siderisstil@gmail.com>
2023-04-29 18:07:36 +0300

    Update scancode_decoder.v
    me vga_clk

--- scancode_decoder.v
+++ scancode_decoder.v
@@ -1,15 +1,16 @@
-module scancode_decoder(flag,scancode,start_address_out,char_enable);
+module scancode_decoder(vga_clk flag,scancode,start_address_out,char_enable);
 input [7:0] scancode;
-input flag;
+input vga_clk,flag;
 output reg char_enable;
 output reg [5:0] start_address_out;
 
 
 //edo kati lipi
-always @(posedge flag ) 
+always @(posedge vga_clk ) 
 begin //to be replaced with vga_clk
-char_enable<=0;
+
 if(flag)
+   char_enable<=0;
    begin
     case(scancode)
         8'h2B: begin //F 
