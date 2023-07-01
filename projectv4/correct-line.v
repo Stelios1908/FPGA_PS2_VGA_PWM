6c27647cbfab315d6acc8c4059b9522653c0b395
Stylianos Sideris <siderisstil@gmail.com>
2023-07-01 20:22:09 +0300

    Delete projectv4 directory

--- projectv4/correct-line.v
+++
@@ -1,9 +0,0 @@
-module correct_line(no_line,display_area,starting_address,address_output);
-input [5:0]starting_address;
-input [3:0]no_line;
-input display_area;
-output [5:0]address_output;
-
-//an den eimasrte sthn energo perioxi bgazv 0 gia mayro
-assign address_output=(display_area) ? starting_address+no_line:0;
-endmodule
